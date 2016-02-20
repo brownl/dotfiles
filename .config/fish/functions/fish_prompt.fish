7acec07669d9fb2cd8f2d0e6e95552a5986648b6
Lawrence Brown <lawrencebrown@zoho.com>
2016-02-20 22:02:22 +0000

    change to a slightly more traditional prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -67,5 +67,6 @@ function fish_prompt
     end
   end
 
-  echo -n -s $arrow ' '$cwd $repo_info $normal ' '
+  # echo -n -s $arrow ' '$cwd $repo_info $normal ' '
+  echo -n -s $cwd $repo_info $normal ' ' $arrow ' '
 end
